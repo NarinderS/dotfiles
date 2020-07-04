92154ac579aa83b4b99e098296bb717d59901b1e
Narinder Singh <narindersingh9930@gmail.com>
2020-07-03 20:17:54 -0600

    Fri_Jul__3_20:17:54_2020

---
+++ polybar/launch.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import subprocess
+import time
+from typing import List
+import re
+import os
+
+def monitorList() -> List[str]:
+    print("monitorList")
+    out = subprocess.check_output(["polybar", "--list-monitors"]).decode("UTF-8")
+    return [i[:-1] for i in re.findall(r".*:", out)]
+
+subprocess.run(['killall', '-q', 'polybar'])
+
+with open("/tmp/polybar.log", 'a') as out:
+    out.write("---------\n")
+    out.write(f"Launching Polybar {time.asctime()}\n")
+    for i, m in enumerate(monitorList()):    
+        subprocess.Popen(['polybar', '-r', 'bspwm'], env=dict(os.environ, MONITOR=m), stdout=out, stderr=out)
+    out.write(f"Bars launched\n")
